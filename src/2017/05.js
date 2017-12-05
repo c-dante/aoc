c315c7f682522257ca3f827f74556d8c6a611ec9
dante <c.dante.federici@gmail.com>
2017-12-05 00:30:58 -0500

    Fixed lower end compare.

--- src/2017/05.js
+++ src/2017/05.js
@@ -1,6 +1,6 @@
 const jumpPc = (strip, idx = 0) => {
 	let steps = 0;
-	while (idx > 0 && idx < strip.length) {
+	while (idx >= 0 && idx < strip.length) {
 		const next = strip[idx];
 		strip[idx]++;
 		idx += next;
