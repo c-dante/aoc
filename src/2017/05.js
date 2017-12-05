6b50c3e29dee224ad3dd3982525b807e8c2f3a2a
dante <c.dante.federici@gmail.com>
2017-12-05 00:35:02 -0500

    Updated for part 2.

--- src/2017/05.js
+++ src/2017/05.js
@@ -1,11 +1,14 @@
-const jumpPc = (strip, idx = 0) => {
+const jumpPc = adjuster => (strip, idx = 0) => {
 	let steps = 0;
 	while (idx >= 0 && idx < strip.length) {
 		const next = strip[idx];
-		strip[idx]++;
+		strip[idx] = adjuster(strip[idx], idx, steps);
 		idx += next;
 		steps++;
 	}
 	return steps;
 };
 
+const jumpPcPart1 = jumpPc(x => x + 1);
+
+const jumpPcPart2 = jumpPc(x => (x >= 3 ? x - 1 : x + 1));
