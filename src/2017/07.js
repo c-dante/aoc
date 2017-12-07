9b593d73ea4e3ff3d95403f2217b846908314d0a
Dante <c.dante.federici@gmail.com>
2017-12-07 00:10:58 -0500

    Create 07.js

---
+++ src/2017/07.js
@@ -0,0 +1,18 @@
+const testInput = `
+pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)`;
+
+const testAnswer = 'tknk';
+
+// This is cute -- We're given [parent] -> [children] links and we're asked to build the DAG :3
