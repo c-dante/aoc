37a189fe52cb7912549b558a1bf034b3334fe2fe
dante <c.dante.federici@gmail.com>
2017-12-12 00:59:28 -0500

    npm test flagging all as slow to see test time.

---
+++ src/2017/11.spec.js
@@ -0,0 +1,17 @@
+// import { assert } from 'chai';
+// import * as src from './x';
+// import problemInput from './x.data';
+
+// describe('--- Day 11: Hex Ed ---', () => {
+// 	describe('Part 1', () => {
+// 		it('should work for test data');
+		
+// 		it('should work for problem input');
+// 	});
+	
+// 	describe('Part 2', () => {
+// 		it('should work for test data');
+
+// 		it('should work for problem input');
+// 	});
+// })
