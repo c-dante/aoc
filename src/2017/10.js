6a872b6dfdcb61197b19a20425273e93926daaf4
dante <c.dante.federici@gmail.com>
2017-12-12 00:54:22 -0500

    Part 2.

--- src/2017/10.js
+++ src/2017/10.js
@@ -2,24 +2,51 @@ import fp from 'lodash/fp';
 
 export const parseInput = input => input.split(',').map(x => +x);
 
-export const part1 = (ringSize, lengths) => {
-	const res = lengths.reduce(
-		(acc, length, skip) => {
-			const offset = acc.cursor + length;
-			for (let i = 0; i < Math.floor(length / 2); i++) {
-				const toSwapIdx = (offset - i - 1) % acc.ring.length;
-				const swapWith = (acc.cursor + i) % acc.ring.length;
-				const x = acc.ring[toSwapIdx];
-				acc.ring[toSwapIdx] = acc.ring[swapWith];
-				acc.ring[swapWith] = x;
-			}
-			acc.cursor = (acc.cursor + length + skip) % acc.ring.length;
-			return acc;
-		},
-		{ cursor: 0, ring: fp.range(0, ringSize) }
-	);
+const hashState = ring => ({ cursor: 0, ring, skip: 0 });
+const hashFn = (acc, length) => {
+	const offset = acc.cursor + length;
+	for (let i = 0; i < Math.floor(length / 2); i++) {
+		const toSwapIdx = (offset - i - 1) % acc.ring.length;
+		const swapWith = (acc.cursor + i) % acc.ring.length;
+		const x = acc.ring[toSwapIdx];
+		acc.ring[toSwapIdx] = acc.ring[swapWith];
+		acc.ring[swapWith] = x;
+	}
+	acc.cursor = (acc.cursor + length + acc.skip) % acc.ring.length;
+	acc.skip++;
+	return acc;
+};
 
+export const part1 = (ringSize, lengths) => {
+	const res = lengths.reduce(hashFn, hashState(fp.range(0, ringSize)));
 	return res.ring[0] * res.ring[1];
 };
 
-// export const part2 = input => {};
+export const denseHash = sparseHash => sparseHash.reduce(
+	(acc, x, i) => {
+		acc.working ^= x;
+		if (i % 16 === 15) {
+			acc.hash.push(acc.working);
+			acc.working = 0;
+		}
+		return acc;
+	},
+	{ hash: [], working: 0 }
+);
+
+export const toHexChar = x => {
+	const str = x.toString(16);
+	return str.length === 1 ? `0${str}` : str;
+}
+export const toHex = arr => arr.reduce((acc, x) => acc.concat(toHexChar(x)), '');
+
+export const part2 = input => {
+	const salt = [17, 31, 73, 47, 23];
+	const lengths = input.trim().split('').map(x => +x.charCodeAt(0)).concat(salt);
+	let acc = hashState(fp.range(0, 256));
+	for (let i = 0; i < 64; i++) {
+		acc = lengths.reduce(hashFn, acc);
+	}
+
+	return toHex(denseHash(acc.ring).hash);
+};
--- src/2017/10.spec.js
+++ src/2017/10.spec.js
@@ -6,18 +6,38 @@ describe('--- Day 10: Knot Hash ---', () => {
 	describe('Part 1', () => {
 		it('should work for test data', () => {
 			const testInput = [3, 4, 1, 5];
-			assert(src.part1(5, testInput), 12);
+			assert.equal(src.part1(5, testInput), 12);
 		});
 		
 		it('should work for problem input', () => {
 			const parsed = src.parseInput(problemInput);
-			assert(src.part1(256, parsed), 11413);
+			assert.equal(src.part1(256, parsed), 11413);
 		});
 	});
 	
 	describe('Part 2', () => {
-		it('should work for test data');
+		it('should dense hash', () => {
+			const res = src.denseHash([65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22]);
+			assert.equal(res.hash[0], 64);
+			
+			const resB = src.denseHash([65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22,11]);
+			assert.equal(resB.hash[0], 64);
+			assert.equal(resB.working, 11);
+		});
 
-		it('should work for problem input');
+		it('can hexify', () => {
+			assert.equal(src.toHex([64, 7, 255]), '4007ff');
+		})
+
+		it('should work for test data', () => {
+			assert.equal(src.part2(''), 'a2582a3a0e66e6e86e3812dcb672a272');
+			assert.equal(src.part2('AoC 2017'), '33efeb34ea91902bb2f59c9920caa6cd');
+			assert.equal(src.part2('1,2,3'), '3efbe78a8d82f29979031a4aa0b16a9d');
+			assert.equal(src.part2('1,2,4'), '63960835bcdc130f0b66d7ff4f6a5a8e');
+		});
+
+		it('should work for problem input', () => {
+			assert.equal(src.part2(problemInput), '7adfd64c2a03a4968cf708d1b7fd418d');
+		});
 	});
 })
